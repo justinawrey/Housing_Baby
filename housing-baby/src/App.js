c58e2d4ecdc48c359b82b76f73f47665512ec538
Steven Shin <steven.shin.95@gmail.com>
2019-01-26 17:40:42 -0800

    app.js

--- housing-baby/src/App.js
+++ housing-baby/src/App.js
@@ -1,37 +1,43 @@
 /*global chrome*/
 
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-
-chrome.storage.sync.set({'address': '4644 West 15th'}, function() {
-  console.log('Value is set to ' + '4644 West 15th');
-});
-
-chrome.storage.sync.get(['address'], function(result) {
-  console.log(result);
-  console.log('Value currently is ' + result.key);
-});
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
+  componentDidMount() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { action: 'GET_ADDRESS' },
+        response => {
+          if (response.address) {
+            console.log(response.address)
+          } else {
+            console.log('ADDRESS NOT AVAILABLE')
+          }
+        }
+      )
+    })
+  }
+
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <p>
-            Edit shit <code>src/App.js</code> and save to reload.
-          </p>
+            Edit shit <code> src / App.js </code> and save to reload.{' '}
+          </p>{' '}
           <a
             className='App-link'
             href='https://reactjs.org'
             target='_blank'
             rel='noopener noreferrer'
           >
-            Learn React
-          </a>
-        </header>
+            Learn React{' '}
+          </a>{' '}
+        </header>{' '}
       </div>
     )
   }
